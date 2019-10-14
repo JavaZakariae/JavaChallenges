27b1a6ca5c8279b2ad57162b6e334bb55cff8424
Bouali Zakariae <gocho_kaka@hotmail.com>
2019-10-14 21:52:48 +0200

    Challenge 12 and 13. Integer class specificity, Function, BiFunction Class from java8

---
+++ src/main/java/Challenge_12.java
@@ -0,0 +1,28 @@
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ *  to be reviewed
+ *
+ */
+public class Challenge_12 {
+
+    static BiFunction<Integer,Integer,Integer> heisenberg = (a,b) -> a+b;
+
+    static Function<Integer,Integer> jesse = a -> a-2;
+
+    static BiFunction<Integer,Integer,Integer> saul =
+                                                heisenberg.andThen(jesse);
+
+    static BiConsumer<Integer, Integer> gustavo =
+                                          (a,b) -> System.out.println(a+2/b+2);
+
+    public static void main( String[] args ) {
+        var firstLaunder = saul.apply(2,2);
+        heisenberg.andThen(jesse);
+
+        var secondLaunder = heisenberg.apply(2,2);
+        gustavo.accept(firstLaunder, secondLaunder);
+    }
+}
