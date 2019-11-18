8cc59bebd59cfc7902564a3931a4adf4a0b72d96
bouali zakariae <bouali_zakariae@hotmail.fr>
2019-11-18 20:35:13 +0100

    Challenge 45. valid identifier

---
+++ src/main/java/challenge41_50/Challenge_45.java
@@ -0,0 +1,22 @@
+package challenge41_50;
+
+/**
+ * valid identifier can begin by underscore, $.
+ * String, Double are valid variable identifier;
+ */
+public class Challenge_45 {
+    native void execute();
+    public static void main( String[] args ) {
+        double _78934 = 1.d;
+        float $78934 = 1.0f;
+        String String = "Game of Thrones";
+        Double Double = 9.d;
+        Float Float = 9.f;
+
+        double doubleX = 9.f;
+        char _98 = 2;
+        for (;; System.out.println("James")) {
+            System.out.println("Tyron");
+        }
+    }
+}
